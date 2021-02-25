5d1dc1887b01fac38d0573a9924790d0f19a1ef3
evgeny32 <evgeny-nosikov@yandex.ru>
2021-02-25 22:16:40 +0300

    Update bee-check.sh

--- bee-check.sh
+++ bee-check.sh
@@ -14,25 +14,13 @@ then
     na=$( curl -s localhost:1635/addresses | jq .ethereum | tr -d '"')
     cba=$(curl -s localhost:1635/chequebook/address | jq .chequebookaddress | tr -d '"')
     echo "Your address: ${bold}$na${normal}"
-    echo "  Etherscan https://goerli.etherscan.io/address/$na"
+    echo "https://goerli.etherscan.io/address/$na"
     echo "Your chequebook: ${bold}$cba${normal}"
-    echo "  Etherscan https://goerli.etherscan.io/address/$cba"
+    echo "https://goerli.etherscan.io/address/$cba"
     echo ""
-    echo -e "Bee node is ${COLOR_GREEN}running :)${COLOR_NC}"
-    systemctl status bee | grep 'Active:'
+    echo -e "${COLOR_GREEN}🐝${COLOR_NC} Bee node is running"
     echo -n "Peers now: "; curl -s http://localhost:1635/peers | jq '.peers | length'
-    echo ""
-    df -h | grep /dev/vda
+    echo -n "Disc space:"; df -h | grep /dev/vda
 else
-    echo -e "Bee node is ${COLOR_RED}NOT RUNNING! :(${COLOR_NC}"
-    read -p "Do you want try to start node? (Yy/n)" -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-      systemctl start bee
-      echo "Please, wait 30sec and run this utility again..."
-      echo "Good Beee!"
-      echo "Bzzz..."
-      exit 0
-    fi
+    echo -e "${COLOR_RED}🐝${COLOR_NC} Bee node is NOT RUNNING!"
 fi
